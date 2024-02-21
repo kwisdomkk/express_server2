4fce20484febf3124c9a60b09a5e1b9de4e84bb3
jihye <kwisdom95@naver.com>
2024-02-21 12:00:34 +0900

    re

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
-import 'dotenv/config';
+import "dotenv/config";
 import "./db";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import appleRouter from './routers/appleRouter';
-import noticeRouter from './routers/noticeRouter';
+import appleRouter from "./routers/appleRouter";
+import noticeRouter from "./routers/noticeRouter";
 
-const corsOptions={
-  origin:["http://localhost:5172","http://localhost:5173","http://localhost:3000"]
-}
-const PORT=process.env.PORT;
+const corsOptions = {
+  origin: [
+    "http://localhost:5172",
+    "http://localhost:5173",
+    "http://localhost:3000",
+  ],
+};
+const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors());
+app.use(cors(corsOptions));
 
-// const middle=(req,res,next)=>{
-//   console.log(`${req.method} ${req.url}`);
-//   next();
-// };
-// app.use(middle); //이거 대신 morgan 설치했음
+app.get("/", (req, res) => res.send({ name: "kenJo" }));
+app.use("/notice", noticeRouter);
+app.use("/apple", appleRouter);
 
-app.get("/",(req,res)=>res.send({name:"je"}));
-app.use("/apple",appleRouter);
-app.use("/notice",noticeRouter)
-
-app.listen(PORT , () => console.log(`http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
