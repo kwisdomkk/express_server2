190ad433da2666211225535890928bb08ad3f7a4
jihye <kwisdom95@naver.com>
2024-04-01 09:09:05 +0900

    dd

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ import morgan from "morgan";
 import cors from "cors";
 import appleRouter from './routers/appleRouter';
 import noticeRouter from './routers/noticeRouter';
+import userRouter from './routers/userRouter';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+
 
 const corsOptions={
-  origin:["http://localhost:5172","http://localhost:5173","http://localhost:3000","https://illustrious-pavlova-3cdb7b.netlify.app"]
+  origin:["http://localhost:5172","http://localhost:5173","http://localhost:3000","https://illustrious-pavlova-3cdb7b.netlify.app"],
+  credentials:true,
 }
 const PORT=process.env.PORT;
 const app = express();
@@ -15,6 +20,21 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors(corsOptions));
+app.use(session({
+  name: "Session ID",
+  secret: "secret",
+  resave: false, // 세션이 변경되지 않아도 항상 저장하도록 설정
+  saveUninitialized: false, // 초기화되지 않은 세션을 저장소에 저장하지 않도록 설정
+  cookie: {
+    maxAge: 1000*60*60*24,
+    httpOnly: true, // javascript에서 사용이 안되게 하는 옵션
+    secure: false, // https를 통해서만 세션 쿠키를 전송하도록 설정
+    },
+    store:MongoStore.create({
+      mongoUrl:process.env.DB_URL+"/yj4-express",
+    })
+  })
+);
 
 // const middle=(req,res,next)=>{
 //   console.log(`${req.method} ${req.url}`);
@@ -24,7 +44,8 @@ app.use(cors(corsOptions));
 
 app.get("/",(req,res)=>res.send({name:"je"}));
 app.use("/apple",appleRouter);
-app.use("/notice",noticeRouter)
+app.use("/notice",noticeRouter);
+app.use("/users",userRouter)
 
 app.listen(PORT , () => console.log(`http://localhost:${PORT}`));
 
